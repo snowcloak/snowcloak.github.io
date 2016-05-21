7ae52414e7eb2c18815e44d47f8cdeeff3e40d66
Miguel Zeng <migzeng27@gmail.com>
2016-05-20 23:44:59 -0400

    Add files via upload

---
+++ Writer.rb
@@ -0,0 +1,40 @@
+require 'date'
+
+#get layout
+#puts "Is this a poem, narrative or drama?"
+type = "narrative"
+
+#get title
+puts "Please enter the full title of the composition"
+title = gets
+title2 = title.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+
+#get author
+puts "Please enter the author"
+author = gets
+
+#get body of work
+puts "Enter the body of text"
+texts = gets
+
+current_date = Date.today
+current_date.strftime("%Y-%m-%d")
+current_date.to_s
+
+source = "#{current_date}" + "-" + title2
+
+f  = File.new("./" + source + ".md", "w")
+f.puts("---")
+f.puts("layout: " + type)
+f.puts("title: "  + %Q{"#{title}"})
+f.puts("author: " + author)
+f.puts("---")
+
+#default conditions
+f.puts(%Q|<a id="title-page" />|)
+
+#write text
+f.puts(texts)
+
+
+f.close
